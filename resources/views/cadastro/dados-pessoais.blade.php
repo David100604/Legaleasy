50804d6023889647885026f56aea9f237c511786
Heitor Ferreira <heitorferreira877@gmail.com>
2024-05-23 10:28:57 -0300

    Atualização da tela de Dados pessoais

--- resources/views/cadastro/dados-pessoais.blade.php
+++ resources/views/cadastro/dados-pessoais.blade.php
@@ -5,15 +5,23 @@
     <section id="conteudo" class="d-flex flex-column justify-content-center">
         <div class="row justify-content-center">
             <img id="linha-branca-esquerda" class="align-self-center col-md-4" src="../images/linha-branca.png" alt="">
-            <h1 id="titulo-heitor" class="align-text-center col-md-4">CADASTRAR NOVA CONTA - DADOS PESSOAIS</h1>
+            <h1 id="titulo-heitor" class="align-text-center col-md-4">CADASTRAR DADOS PESSOAIS</h1>
             <img id="linha-branca-direita-heitor" class="align-self-center col-md-4" src="../images/linha-branca.png" alt="">
         </div>
-        <div class="row">
-            <p class="col-md-12 align-text-center">
+        <div id="info-cadastro-heitor">
+            <p class="text-center">
                 Para fazer o uso da nossa plataforma é necessário o cadastro de seus dados como cliente
-                ou prestador de serviço. Preencha o formulário abaixo com dados válidos para criar uma conta.
+                ou prestador de serviço. 
+                <br>Preencha o formulário abaixo com dados válidos para criar uma conta.
                 Os campos marcados com * são obrigatórios.
             </p>
+        </div>
+        <div id="info-cadastro-heitor">
+            <h3 class="text-left" id="subtitulo-heitor" class="align-text-center col-md-4">Insira suas informações nos campos abaixo:</h3>
+        </div>
+
+        <div id="inputs-cadastro" class="justify-content">
+
         </div>
     </section>
 
