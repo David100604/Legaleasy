3966bf5068c289ebaff1ad0582c9dc284f4f5627
Heitor Ferreira <heitorferreira877@gmail.com>
2024-05-23 08:22:07 -0300

    Criado a tela de Dados Pessoais

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,11 @@ Route::get('/', function () {
     return view('cadastro.nova-conta');
 });
 
+Route::get('/dadospessoais', function () {
+    return view('cadastro.dados-pessoais');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
