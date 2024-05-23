57383a05a8a04a49ba7b84eec20eb3e64181f5e3
David <davidvn1006@gmail.com>
2024-05-23 08:19:05 -0300

    Iniciando rela de nova conta

--- resources/views/cadastro/nova-conta.blade.php
+++ resources/views/cadastro/nova-conta.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
 
-    <h1>Teste</h1>
+    <section id="conteudo" class="row d-flex justify-content-center">
+        <img id="linha-branca-esquerda" class="align-self-center col-sm-4" src="../images/linha-branca.png" alt="">
+        <h1 class="align-self-center col-sm-4">Cadastrar Nova Conta</h1>
+        <img id="linha-branca-direita" class="align-self-center col-sm-4" src="../images/linha-branca.png" alt="">
+    </section>
 
 @endsection
