069d45a60d1f4864bd85e46148f19b018f3ddb26
David <davidvn1006@gmail.com>
2024-05-23 08:36:19 -0300

    Editando fonte

--- resources/views/cadastro/nova-conta.blade.php
+++ resources/views/cadastro/nova-conta.blade.php
@@ -2,10 +2,15 @@
 
 @section('content')
 
-    <section id="conteudo" class="row d-flex justify-content-center">
-        <img id="linha-branca-esquerda" class="align-self-center col-sm-4" src="../images/linha-branca.png" alt="">
-        <h1 class="align-self-center col-sm-4">Cadastrar Nova Conta</h1>
-        <img id="linha-branca-direita" class="align-self-center col-sm-4" src="../images/linha-branca.png" alt="">
+    <section id="conteudo" class="d-flex flex-column justify-content-center">
+        <div class="row justify-content-center">
+            <img id="linha-branca-esquerda" class="align-self-center col-md-4" src="../images/linha-branca.png" alt="">
+            <h1 id="titulo" class="align-text-center col-md-4">CADASTRAR NOVA CONTA</h1>
+            <img id="linha-branca-direita" class="align-self-center col-md-4" src="../images/linha-branca.png" alt="">
+        </div>
+        <div class="row">
+            <p class="col-md-12 align-text-center">Escolha o tipo de conta que deseja cadastrar. Campos de informação adicionais podem ser </p>
+        </div>
     </section>
 
 @endsection
