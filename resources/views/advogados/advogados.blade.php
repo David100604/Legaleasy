b75fc7c40ac944a01f029ef172e7bc8632a3d4f8
Rhuan Bryan <rhuanbryan545@gmail.com>
2024-05-29 20:46:14 -0300

    Adicionando estrelas de avaliação

--- resources/views/advogados/advogados.blade.php
+++ resources/views/advogados/advogados.blade.php
@@ -52,7 +52,18 @@
                 <div class="row">
                     <div class="col-6" style="border-right: 1px solid gray;">
                         <p class="m-0 nome-advogado">Nome do Advogado</p>
-                        <p class="m-0">Estrelas (À fazer)</p>
+                        <div class="star-rating">
+                            <input type="radio" id="star5" name="rating" value="5" />
+                            <label for="star5" title="5 estrelas">☆</label>
+                            <input type="radio" id="star4" name="rating" value="4" />
+                            <label for="star4" title="4 estrelas">☆</label>
+                            <input type="radio" id="star3" name="rating" value="3" />
+                            <label for="star3" title="3 estrelas">☆</label>
+                            <input type="radio" id="star2" name="rating" value="2" />
+                            <label for="star2" title="2 estrelas">☆</label>
+                            <input type="radio" id="star1" name="rating" value="1" />
+                            <label for="star1" title="1 estrela">☆</label>
+                        </div>
                         <p class="m-0">Número de avaliações: 7</p>
                         <p class="m-0">Serviços: Advogado Civil</p>
                     </div>
@@ -71,7 +82,18 @@
                 <div class="row">
                     <div class="col-6" style="border-right: 1px solid gray;">
                         <p class="m-0 nome-advogado">Nome do Advogado</p>
-                        <p class="m-0">Estrelas (À fazer)</p>
+                        <div class="star-rating">
+                            <input type="radio" id="star5" name="rating" value="5" />
+                            <label for="star5" title="5 estrelas">☆</label>
+                            <input type="radio" id="star4" name="rating" value="4" />
+                            <label for="star4" title="4 estrelas">☆</label>
+                            <input type="radio" id="star3" name="rating" value="3" />
+                            <label for="star3" title="3 estrelas">☆</label>
+                            <input type="radio" id="star2" name="rating" value="2" />
+                            <label for="star2" title="2 estrelas">☆</label>
+                            <input type="radio" id="star1" name="rating" value="1" />
+                            <label for="star1" title="1 estrela">☆</label>
+                        </div>
                         <p class="m-0">Número de avaliações: 7</p>
                         <p class="m-0">Serviços: Advogado Civil</p>
                     </div>
@@ -90,7 +112,18 @@
                 <div class="row">
                     <div class="col-6" style="border-right: 1px solid gray;">
                         <p class="m-0 nome-advogado">Nome do Advogado</p>
-                        <p class="m-0">Estrelas (À fazer)</p>
+                        <div class="star-rating">
+                            <input type="radio" id="star5" name="rating" value="5" />
+                            <label for="star5" title="5 estrelas">☆</label>
+                            <input type="radio" id="star4" name="rating" value="4" />
+                            <label for="star4" title="4 estrelas">☆</label>
+                            <input type="radio" id="star3" name="rating" value="3" />
+                            <label for="star3" title="3 estrelas">☆</label>
+                            <input type="radio" id="star2" name="rating" value="2" />
+                            <label for="star2" title="2 estrelas">☆</label>
+                            <input type="radio" id="star1" name="rating" value="1" />
+                            <label for="star1" title="1 estrela">☆</label>
+                        </div>
                         <p class="m-0">Número de avaliações: 7</p>
                         <p class="m-0">Serviços: Advogado Civil</p>
                     </div>
