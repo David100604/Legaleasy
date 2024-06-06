2f12df2c6929bd429fe9bfc26f31ff55efbc6d60
David <davidvn1006@gmail.com>
2024-06-06 08:20:41 -0300

    Alterando botão de voltar

--- resources/views/casos/registrar-caso.blade.php
+++ resources/views/casos/registrar-caso.blade.php
@@ -134,9 +134,9 @@
 
             <div class="row justify-content-center align-self-end my-5 mt-md-5">
                 <img class="linha-branca align-self-center d-none d-xl-block" src="../images/linha-branca.png" alt="">
-                <button class="align-self-center col-xl-2 col-7 m-1 py-1 py-sm-2 btn btn-dark">
+                <a class="align-self-center col-xl-2 col-7 m-1 py-1 py-sm-2 btn btn-dark" href="javascript:window.history.go (-1)">
                     VOLTAR
-                </button>
+                </a>
                 <button class="btn-azul align-self-center col-xl-2 col-7 m-1 py-1 py-sm-2 btn">
                     CADASTRAR
                 </button>
