14686e641811feebd01c7f5d4e295f0de7e303eb
Ricardo Lima <ricardolimasouza27@gmail.com>
2024-06-06 09:17:04 -0300

    Alterações na tela de caso

--- resources/views/casos/caso.blade.php
+++ resources/views/casos/caso.blade.php
@@ -31,29 +31,20 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4">
-                <div class="card-btn">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">Foto_documento.png</h5>
-                        <a href="#" class="btn btn-primary" target="_blank">Visualizar</a>
-                        <a href="#" class="btn btn-secondary" download>Fazer download</a>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
     <div class="row justify-content-center align-self-end my-3 mt-md-5">
-    <img class="linha-branca align-self-center d-none d-xl-block" src="{{ asset('images/linha-branca.png') }}" alt="">
-    <a class="align-self-center col-xl-2 col-7 m-1 py-1 py-sm-2 btn btn-dark" href="javascript:window.history.go (-1)">
+    <img class="linhamenor align-self-center d-none d-xl-block mx-5" src="{{ asset('images/linhamenordireita.png') }}" alt="">
+    <a class="align-self-center col-xl-2 col-7 m-1 py-1 py-sm-2 btn btn-dark " href="javascript:window.history.go (-1)">
         VOLTAR
     </a>
     <button type="submit" class="btn-azul align-self-center col-xl-2 col-7 m-1 py-1 py-sm-2 btn">
         PRÓXIMO
     </button>
-    <button type="submit" class="btn-branca align-self-center col-xl-2 col-7 m-1 py-1 py-sm-2 btn">
+    <button type="submit" class="btn-branca align-self-center col-xl-2 col-7 m-1 py-1 py-sm-2   btn">
         INCIAR CONVERSA
     </button>
-    <img class="linha-branca align-self-center d-none d-xl-block" src="{{ asset('images/linha-branca-direita.png') }}" alt="">
+    <img class="linhamenor align-self-center d-none d-xl-block mx-5" src="{{ asset('images/linhamenor.png') }}" alt="">
 </div>
 
 </div>
